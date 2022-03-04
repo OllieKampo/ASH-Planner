2b77e11bf76c53c5af2dd18aa0feffb2d965de01
Oliver Kamperis <olliekampo@gmail.com>
2022-03-04 20:11:11 +0000

    Preach experiments complete. Time scores and grades need correcting for initials and preach experiments.

--- experiments/RemoveDateTime.ps1
+++ experiments/RemoveDateTime.ps1
@@ -1,3 +1,3 @@
 param([Parameter(Mandatory=$True, Position=0, HelpMessage="Path to folder containing experimental results.")] [string] $path)
 
-Get-ChildItem -Path "$($path)\*" -File | Rename-Item -NewName { $_.Name -replace "_([0-9-])*_([0-9-])*_", "_" }
+Get-ChildItem -Path "$($path)\*" -File | Rename-Item -NewName { $_.Name -replace "_([0-9-]){10}_([0-9-]){8}_", "_" }
